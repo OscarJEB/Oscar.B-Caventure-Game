cf99525cae0e8fbfe87f522785d01e31b70bbfcb
OscarJEB <burgeo1@macstudent.com.au>
2025-05-26 01:57:44 +0000

    added link cave attribute and descriptios for caves. Still need to add descriptions for other 13 caves.

--- cave.py
+++ cave.py
@@ -1,7 +1,8 @@
 class Cave:
     def __init__(self, cave_name):
         self.name = cave_name
-        self.description = None  
+        self.description = None 
+        self.linked_caves = {} 
 
     def get_description(self):
         return self.description
@@ -9,7 +10,9 @@ class Cave:
     def set_description(self, cave_description):
         self.description = cave_description
 
-
     def describe(self):
         print(self.description)
+
+    def link_cave(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
 
