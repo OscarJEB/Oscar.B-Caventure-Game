e7dce600c1be4f3c1bbe75878f36b5f89bd904c0
OscarJEB <burgeo1@macstudent.com.au>
2025-05-29 00:00:37 +0000

    got up to step 18 but issue i had is commented on doc

--- cave.py
+++ cave.py
@@ -10,9 +10,22 @@ class Cave:
     def set_description(self, cave_description):
         self.description = cave_description
 
+    def get_name(self, cave_name):
+        return self.name
+
+    def set_name(self, cave_name):
+        self.name = cave_name
+
+
     def describe(self):
         print(self.description)
 
     def link_cave(self, cave_to_link, direction):
         self.linked_caves[direction] = cave_to_link
+
+    def get_details(self):
+        for direction in self.linked_caves:
+            cave = self.linked_caves[direction]
+            print( "The " + cave.get_name() + " is " + direction)
+
 
